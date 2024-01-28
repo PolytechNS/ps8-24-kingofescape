8fe5832a630af4579a5a04e516e794745ea22cf7
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2024-01-28 10:21:25 +0100

    task: Ajout d'un Mur
    Ajout de la classe mur pour pouvoir poser des mur dans le jeu

--- front/wall.js
+++ front/wall.js
@@ -1,7 +1,15 @@
-export class Wall {
+class Wall {
     constructor(coordinate1, coordinate4, isVertical) {
+        console.log(coordinate1, coordinate4, isVertical);
+        if (coordinate1.x !== (coordinate4.x - 1) && coordinate1.y !== (coordinate4.y - 1)) {
+            console.log(coordinate1.x, coordinate4.x, coordinate1.y, coordinate4.y);
+            throw new Error("Les coordonnées ne sont pas alignées");
+        }
+
         this.coordinate1 = coordinate1;
         this.coordinate4 = coordinate4;
         this.isVertical = isVertical;
     }
 }
+
+exports.Wall = Wall;
