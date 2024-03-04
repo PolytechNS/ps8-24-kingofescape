24e502a6290303f024a0afef6b11230fc43f14bd
amandinemart1 <amandine-martin@sfr.fr>
2024-03-04 17:29:56 +0100

    score requtes by adding and getting from a user

---
+++ back/1v1/score.js
@@ -0,0 +1,41 @@
+const {MongoClient} = require('mongodb');
+const {urlAdressDb} = require('../env/env.js');
+const jwt = require('jsonwebtoken');
+
+async function getScores(json, response) {
+    const client = new MongoClient(urlAdressDb);
+    client.connect();
+    const users = client.db('sample_mflix');
+
+    try {
+        const username = json.username;
+        const scores = await users.collection("Scores").find({username: username}).toArray();
+        response.statusCode = 200;
+        response.end(JSON.stringify(scores));
+    } catch (error) {
+        console.log(error);
+        response.statusCode = 500;
+        response.end('Error getting scores');
+    }
+}
+
+async function addScore(json, response) {
+    const client = new MongoClient(urlAdressDb);
+    client.connect();
+    const users = client.db('sample_mflix');
+
+    try {
+        const username = json.username;
+        const score = json.score;
+        await users.collection("Scores").insertOne({username: username, score: score});
+        response.statusCode = 200;
+        response.end('Score added');
+    } catch (error) {
+        console.log(error);
+        response.statusCode = 500;
+        response.end('Error adding score');
+    }
+}
+
+exports.addScore = addScore;
+exports.getScores = getScores;
