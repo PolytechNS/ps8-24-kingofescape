d8568afdbff3b12b85b2fff0fb2879c59ff060b9
amandinemart1 <amandine-martin@sfr.fr>
2024-04-04 22:48:17 +0200

    sucess win lose game

--- front/src/sucess/sucess.js
+++ front/src/sucess/sucess.js
@@ -1,74 +1,60 @@
-
-function scoreSucess(){
-    let result = verifyLogin();
+async function fetchData(endpoint) {
+    let result = await verifyLogin();
     if (result !== null) {
-        result.then(async (response) => {
-            if (response.status === 200) {
-                let url = 'http://localhost:8000/api/getScores/'+ await response.text();
-                fetch(url, {
-                    method: 'get'
-                }).then(async (response) => {
-                    if (response.status === 200) {
-                        let res = await response.text();
-                        res = JSON.parse(res);
-                        if (res.score <50) {
-                            changeOpacity('score1');
-                            changeOpacity('score2');
-                            changeOpacity('score3');
-                            changeOpacity('score4');
-                            changeOpacity('score5');
-                            changeOpacity('score6');
-                            changeOpacity('score7');
-                            changeOpacity('score8');
-                        }
-                        else if(res.score <100) {
-                            changeOpacity('score2');
-                            changeOpacity('score3');
-                            changeOpacity('score4');
-                            changeOpacity('score5');
-                            changeOpacity('score6');
-                            changeOpacity('score7');
-                            changeOpacity('score8');
-                        } else if(res.score <150) {
-                            changeOpacity('score3');
-                            changeOpacity('score4');
-                            changeOpacity('score5');
-                            changeOpacity('score6');
-                            changeOpacity('score7');
-                            changeOpacity('score8');
-                        } else if(res.score <200) {
-                            changeOpacity('score4');
-                            changeOpacity('score5');
-                            changeOpacity('score6');
-                            changeOpacity('score7');
-                            changeOpacity('score8');
-                        } else if(res.score <300) {
-                            changeOpacity('score5');
-                            changeOpacity('score6');
-                            changeOpacity('score7');
-                            changeOpacity('score8');
-                        } else if(res.score <500) {
-                            changeOpacity('score6');
-                            changeOpacity('score7');
-                            changeOpacity('score8');
-                        } else if(res.score <750) {
-                            changeOpacity('score7');
-                            changeOpacity('score8');
-                        } else if(res.score <1000) {
-                            changeOpacity('score8');
-                        }
-                    }
-                    else {
-                    }
-                });
+        let response = await fetch(endpoint + await result.text());
+        if (response.status === 200) {
+            return await response.json();
+        }
+    }
+    return null;
+}
+
+async function scoreSuccess() {
+    let res = await fetchData('http://localhost:8000/api/getScores/');
+    if (res !== null && res.score !== undefined) {
+        let score = res.score;
+        let elements = ['score1', 'score2', 'score3', 'score4', 'score5', 'score6', 'score7', 'score8'];
+        let steps = [50,100,150,200,300,500,750,1000];
+        let j = 0; 
+
+        for (let i of steps) {
+            if (score < i) {
+                changeOpacity(elements[j]);
             }
-        });
+            j++;
+        }
     }
 }
 
-function changeOpacity(id){
-    var element = document.getElementById(id); 
-    return element.style.opacity = 0.5; 
+async function gameSuccess() {
+    let res = await fetchData('http://localhost:8000/api/getStat/');
+    if (res !== null && res.win !== undefined) {
+        let win = res.win;
+        let lose = res.lose;
+        let elements = ['sucess_win1', 'sucess_win5', 'sucess_win10', 'sucess_win20', 'sucess_win50', 'sucess_win100', 'sucess_win200', 'sucess_win500', 'sucess_win1000'];
+        let elements2 = ['sucess_loos1', 'sucess_loos5', 'sucess_loos10', 'sucess_loos20', 'sucess_loos50', 'sucess_loos100', 'sucess_loos200', 'sucess_loos500', 'sucess_loos1000'];
+        let steps = [1,5,10,20,50,100,200,500,1000];
+        let j = 0; 
+        console.log(win);
+        console.log(lose);
+        for (let i of steps) {
+            if (win < i) {
+                changeOpacity(elements[j]);
+            } 
+            if (lose < i) {
+                changeOpacity(elements2[j]); 
+            }
+            j++;
+        }
+    }
 }
 
-scoreSucess();
+function changeOpacity(id) {
+    var element = document.getElementById(id);
+    if (element) {
+        element.style.opacity = 0.5;
+    }
+}
+
+scoreSuccess();
+gameSuccess();
