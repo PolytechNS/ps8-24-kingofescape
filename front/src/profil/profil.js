901a2f66559ea36264493eb44a7fcf958c7db1fc
amandinemart1 <amandine-martin@sfr.fr>
2024-04-08 18:36:43 +0200

    deletion account in the front

--- front/src/profil/profil.js
+++ front/src/profil/profil.js
@@ -21,9 +21,27 @@ function loadProfil() {
 function logout() {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
     changePage('mode/mode.html');
+
+}
+async function fetchData(adresse, name) {
+    let response = await fetch(adresse + name, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.status === 200)
+        return response;
+    else
+        return null;
 }
 
-function deleteAccount() {
+async function deleteAccount() {
+    const name = document.getElementById("name").innerHTML;
+    const score = await fetchData('http://localhost:8000/api/deleteScore/', name);
+    const state = await fetchData('http://localhost:8000/api/deleteStat/', name);
+    const account = await fetchData('http://localhost:8000/api/deleteAccount/', name);
+    logout();
 }
 
 loadProfil();
