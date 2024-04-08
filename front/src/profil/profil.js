4301877657aff12e74cd82a70f79352c01d787ac
baidouri <alibaidouri7@gmail.com>
2024-04-08 22:03:28 +0200

    add profile avatar Changes based in scores

--- front/src/profil/profil.js
+++ front/src/profil/profil.js
@@ -1,18 +1,31 @@
-function loadProfil() {
+async function loadProfil() {
     const result = verifyLogin();
 
     if (result !== null) {
         result.then(async (response) => {
             if (response.status === 200) {
                 document.getElementById("name").innerHTML = await response.text();
-            }
-            else {
+                console.log(response.text());
+                let scoresResponse = await fetch('http://localhost:8000/api/getScoresAllUsers', {
+                    method: 'get'
+                });
+                if (scoresResponse.status === 200) {
+                    let res = await scoresResponse.text();
+                    let json = JSON.parse(res);
+                    for (let i = 0; i < json.length; i++) {
+                        if (json[i].username === document.getElementById("name").innerHTML) {
+                            let backgroundImage = getBackgroundImageBasedOnScore(json[i].score);
+                            document.getElementById("imguser").style.backgroundImage = `url('${backgroundImage}')`;
+                            break;
+                        }
+                    }
+                }
+            } else {
                 window.alert("Please connect !");
                 changePage('mode/mode.html')
             }
         });
-    }
-    else {
+    } else {
         window.alert("Please connect !");
         changePage('mode/mode.html')
     }
@@ -35,6 +48,23 @@ async function fetchData(adresse, name) {
     else
         return null;
 }
+function getBackgroundImageBasedOnScore(score) {
+    if (score < 100) {
+        return '../picture/littleprince.png';
+    } else if (score >= 100 && score < 300) {
+        return '../picture/teenprince.png';
+    } else if (score >= 300 && score <= 500) {
+        return '../picture/avatar2.png';
+    } else if (score > 500 && score <= 700) {
+        return '../picture/beforeking.png';
+    }
+    else if (score > 700) {
+        return '../picture/king5.png';
+    }
+}
+
+
+
 
 async function deleteAccount() {
     const name = document.getElementById("name").innerHTML;
