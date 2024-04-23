b9dacf6bc640a94978d1dd719e55e2cc8f8d6c49
baidouri <alibaidouri7@gmail.com>
2024-04-23 10:40:28 +0200

    add notif mode page et modif css

--- front/src/js/FriendRequestsNotifManager.js
+++ front/src/js/FriendRequestsNotifManager.js
@@ -10,11 +10,26 @@ const user = localStorage.getItem('currentUser');
 notificationSocket.on('connect', () => {
     console.log('Connected to the notification server.');
 });
+function changeButtonIcon(notificationReceived) {
+    const button = document.getElementById('buttonNotification');
+    if (button) {
+        if (notificationReceived) {
+            button.style.backgroundImage = "url('../picture/cloche2.png')";
+
+        } else {
+            // Chemin vers l'icône de notification normale
+            button.style.backgroundImage = "url('../picture/notificon.png')";
+
+        }
+    } else {
+        console.error('Button not found');
+    }
+}
+
 notificationSocket.on('notification', function(data) {
     displayNotification(data.message);
-    showNotificationIndicator();
+    changeButtonIcon(true);
 });
-
 function displayNotification(message) {
     const notificationArea = document.getElementById('notification-area');
     if (!notificationArea) {
@@ -27,32 +42,24 @@ function displayNotification(message) {
     notification.textContent = message;
     notificationArea.appendChild(notification);
 
+    // Jouer le son de notification
+    const notificationSound = document.getElementById('notificationSound');
+    notificationSound.play();
+
     notification.addEventListener('click', () => {
         notification.remove();
+        if (notificationArea.children.length === 0) {
+            changeButtonIcon(false); // Réinitialise l'icône lorsque toutes les notifications sont supprimées
+        }
     });
-
-    // Hide the notification indicator when notifications are displayed
-    hideNotificationIndicator();
-}
-function showNotificationIndicator() {
-    const indicator = document.getElementById('notificationIndicator');
-    if (indicator) {
-        indicator.style.display = 'block';
-    }
 }
 
-function hideNotificationIndicator() {
-    const indicator = document.getElementById('notificationIndicator');
-    if (indicator) {
-        indicator.style.display = 'none';
-    }
-}
 
-document.getElementById('notificationButton').addEventListener('click', () => {
+document.getElementById('buttonNotification').addEventListener('click', () => {
     const notificationArea = document.getElementById('notification-area');
     if (notificationArea.style.display === 'none' || !notificationArea.style.display) {
         notificationArea.style.display = 'block';
-        hideNotificationIndicator();
+
     } else {
         notificationArea.style.display = 'none';
     }
